dfeea98788730957b052b519939003ec959ddb85
FredProcimo <frederico.janeira@procimo.com>
2025-07-01 20:53:52 +0100

    Authenticate Requests

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 // Initialize Firebase Admin SDK
 admin.initializeApp();
@@ -9,16 +9,20 @@ export const region = "europe-west1";
 export const db = admin.firestore;
 
 // Export Router Authentication
-export const Authenticate = async (req: Request, res: Response) => {
+export const Authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const header = req.headers.authorization;
     if (!header || !header.startsWith('Bearer ')) {
         res.status(401).json({ error: 'Unauthorized: No token provided' });
         return;
     }
     const token = header.split('Bearer ')[1];
-    if(token === "qa-app-testing") return;
+    if(token === "qa-app-testing") {
+        next(); 
+        return;
+    }
     try {
         await admin.auth().verifyIdToken(token);
+        next();
     } catch (error) {
         res.status(401).json({ error: 'Unauthorized: Invalid token' });
     }
